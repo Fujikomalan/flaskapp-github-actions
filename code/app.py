6687ab21e5dc91094cfa16fc3dcd1a290832a082
fuji komalan <fujikomalan@gmail.com>
2025-08-05 07:44:13 +0000

    version1

---
+++ code/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Create a Flask web application instance
+app = Flask(__name__)
+
+# Define a route for the root URL ('/')
+@app.route('/')
+def hello_world():
+  return '<h1><center>Flask Application Version1</center></h1>'
+
+# Run the application if the script is executed directly
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',port=8080,debug=True)
+
